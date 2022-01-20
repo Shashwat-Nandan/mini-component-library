67736a09bf107082b4921903fede9e07a45b473d
Shashwat Nandan <shashwat.nandan@propelytics.in>
2022-01-20 10:51:18 +0000

    Add progress progress style

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,41 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  large: {
+    "--height": 24 + "px",
+    "--width": 370 + "px",
+    "--borderRadius": 8 + "px"
+  },
+  medium:{
+    "--height": 12 + "px",
+    "--width": 370 + "px",
+    "--borderRadius": 4 + "px"
+  },
+  small: {
+    "--height": 8 + "px",
+    "--width": 370 + "px",
+    "--borderRadius": 4 + "px"
+  }
 };
+
+const ProgressBar = ({ value, size }) => {
+
+  let style = SIZES[size];
+  return <ButtonBase style={style}><FIllButton /></ButtonBase>;
+};
+
+const ButtonBase = styled.div`
+  width: 370px;
+  border: 1px solid ${COLORS.transparentGray15};
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const FIllButton = styled(ButtonBase)`
+  width: 185px;
+  background-color: ${COLORS.primary};
+  padding: 2px;
+`;
 
 export default ProgressBar;
