b2f4613ab15f6d47e2a00392f1c35cd72221c9c8
Shashwat Nandan <shashwat.nandan@propelytics.in>
2022-01-22 14:11:28 +0000

    Add styles for Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,49 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </NativeSelect>
+    
+    <OtherElement>{displayedValue}
+      <ButtonWrapper>
+        <Icon id="chevron-down" size={16}/>
+      </ButtonWrapper>  
+    </OtherElement>
+    </Wrapper>
+    
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  left:0;
+  top:0;
+  width: 100%;
+  height:100%;
+  opacity:0;
+`;
+
+const OtherElement = styled.div`
+ font-size: ${16/16}rem;
+ padding: 12px 16px;
+ padding-right: 52px;
+ color: ${COLORS.gray700};
+ background-color: ${COLORS.transparentGray15};
+ border-radius: 8px;
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  top:0;
+  right:0;
+`;
+
 
 export default Select;
